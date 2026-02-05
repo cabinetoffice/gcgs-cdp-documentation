8f13d92ca7fc951a4f5a6089365dddaa3bc2eaec
Andrew Hirst <andrew.hirst@goaco.com>
2026-02-05 15:14:22 +0000

    Merge config

--- config.rb
+++ config.rb
@@ -5,3 +5,10 @@ base_path = ENV.fetch("BASE_PATH", "").chomp("/")
 set :http_prefix, base_path unless base_path.empty?
 
 GovukTechDocs.configure(self)
+
+# Make links/assets portable (works under subpaths like /gcgs-cdp-documentation/)
+set :relative_links, true
+
+configure :build do
+  activate :relative_assets
+end
