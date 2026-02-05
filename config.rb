7bf5018edeb010204b448a098b9f10c0371d13af
Andrew Hirst <andrew.hirst@goaco.com>
2026-02-05 14:55:35 +0000

    Replace http_prefix with relative_links

--- config.rb
+++ config.rb
@@ -1,7 +1,10 @@
 require 'govuk_tech_docs'
 
-base_path = ENV.fetch("BASE_PATH", "").chomp("/")
-
-set :http_prefix, base_path unless base_path.empty?
-
 GovukTechDocs.configure(self)
+
+# Make links/assets portable (works under subpaths like /gcgs-cdp-documentation/)
+set :relative_links, true
+
+configure :build do
+  activate :relative_assets
+end
