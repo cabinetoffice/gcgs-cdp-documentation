4b4c5202e8393ea0714506fef0ebc5cdc3a85501
Andrew Hirst <andrew.hirst@goaco.com>
2026-02-05 15:49:34 +0000

    Sync CI/CD config from integration

--- config.rb
+++ config.rb
@@ -1,9 +1,5 @@
 require 'govuk_tech_docs'
 
-base_path = ENV.fetch("BASE_PATH", "").chomp("/")
-
-set :http_prefix, base_path unless base_path.empty?
-
 GovukTechDocs.configure(self)
 
 # Make links/assets portable (works under subpaths like /gcgs-cdp-documentation/)
